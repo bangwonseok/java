7c07eadd0ee2f7d0614dc7ac2ef6d8be6e6122c3
bangwonseok <86813429+bangwonseok@users.noreply.github.com>
2021-08-31 18:14:18 +0900

    Add files via upload

---
+++ exam1/src/chapter10_6/Customer.java
@@ -0,0 +1,26 @@
+package chapter10_6;
+
+public class Customer implements Buy, Sell {
+
+	@Override
+	public void buy() {
+		System.out.println("Buy 메서드");
+	}
+
+	@Override
+	public void sell() {
+	
+		
+	}
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return Buy.super.getPrice();
+	}
+
+	@Override
+	public void showInfo() {
+		System.out.println("Sell 인터페이스");
+	}
+}
